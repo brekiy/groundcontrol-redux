db4db5f3a23808949bbfa9a5d903d9b26ff40f9d
afxnatic <174385871+afxnatic@users.noreply.github.com>
2025-11-08 11:56:53 -0800

    rewritten scoreboard in derma

--- gamemodes/groundcontrolredux/gamemode/cl_scoreboard.lua
+++ gamemodes/groundcontrolredux/gamemode/cl_scoreboard.lua
@@ -1,30 +1,22 @@
--- todo: make the scoreboard a derma element
--- this code in general is pretty bad (locals defined outside of functions), was written back in 2012, needs a rewrite
+function GM:SortPlayers(teamEnum)
+    local players = team.GetPlayers(teamEnum)
+    local sorted = {}
 
-local clr, rect, orect, stext, lgrad = surface.SetDrawColor, surface.DrawRect, surface.DrawOutlinedRect, draw.ShadowText, draw.LinearGradient
-local lp, X, Y, X2, Y2, ply, scale1, scale2, state, statecolor, dtint1, ktext, kcolor, FT, slot, blue, red, f1, f2, n, d1, d2, n1, n2
-local PTBL = {}
+    for _, player1 in ipairs(players) do
+        local score1, nick1, d1 = player1:GetNWInt("GC_SCORE"), player1:Name(), player1:Deaths()
+        local slot = #players
 
-function GM:SortPlayers(t)
-    lp = LocalPlayer()
-    ply = team.GetPlayers(t)
-    table.Empty(PTBL)
+        for _, player2 in ipairs(players) do
+            if player1 != player2 then
+                local score2, nick2, deaths2 = player2:GetNWInt("GC_SCORE"), player2:Name(), player2:Deaths()
 
-    for k, v in pairs(ply) do
-        n1, f1, d1 = v:GetNWInt("GC_SCORE"), v:Name(), v:Deaths()
-        slot = #ply
-
-        for k2, v2 in pairs(ply) do
-            if v != v2 then
-                n2, f2, d2 = v2:GetNWInt("GC_SCORE"), v2:Name(), v2:Deaths()
-
-                if n1 > n2 then
+                if score1 > score2 then
                     slot = slot - 1
-                elseif n1 == n2 then
-                    if d1 < d2 then
+                elseif score1 == score2 then
+                    if deaths1 < deaths2 then
                         slot = slot - 1
-                    elseif d1 == d2 then
-                        if f1 < f2 then
+                    elseif deaths1 == deaths2 then
+                        if nick1 < nick2 then
                             slot = slot - 1
                         end
                     end
@@ -32,18 +24,18 @@ function GM:SortPlayers(t)
             end
         end
 
-        PTBL[slot] = v
+        sorted[slot] = player1
     end
 
-    return PTBL
+    return sorted
 end
 
 function GM:ScoreboardShow()
-    self.ShowScoreboard = true
+    self:CreateScoreboard()
 end
 
 function GM:ScoreboardHide()
-    self.ShowScoreboard = false
+    self:DestroyScoreboard()
 end
 
 GM.ScoreboardColors = {
@@ -57,120 +49,211 @@ GM.ScoreboardColors = {
     ColorGray2 = Color(170, 170, 170, 150)
 }
 
-local ttf = team.TotalFrags
-local sleft = string.Left
+local gcScoreboardFrame = {}
 
-local CW_HUD16 = "CW_HUD16"
+function gcScoreboardFrame:Update()
+    if IsValid(self.BluePanel) then
+        self.BluePanel:Update()
+    end
 
-function GM:HUDDrawScoreBoard()
-    if !self.ShowScoreboard then
+    if IsValid(self.RedPanel) then
+        self.RedPanel:Update()
+    end
+end
+
+function gcScoreboardFrame:Paint(w, h)
+    surface.SetDrawColor(0, 0, 0, 75)
+    surface.DrawRect(0, 0, w, h)
+
+    return true
+end
+
+function gcScoreboardFrame:PerformLayout(w, h)
+    self.BluePanel:SetSize(w / 2, h)
+    self.RedPanel:SetSize(w / 2, h)
+    self.RedPanel:SetPos(w / 2, 0)
+end
+
+vgui.Register("GCScoreboardFrame", gcScoreboardFrame, "DPanel")
+
+local ROW_HEIGHT_RATIO = 0.045
+
+local gcScoreboardTeamColumn = {}
+AccessorFunc(gcScoreboardTeamColumn, "Team", "Team", FORCE_NUMBER)
+
+function gcScoreboardTeamColumn:Init()
+    self.PlayerRows = {}
+end
+
+function gcScoreboardTeamColumn:Update()
+    for _, panel in ipairs(self.PlayerRows) do
+        if not IsValid(panel) then
+            continue
+        end
+
+        panel:Remove()
+    end
+
+    if not self.Team then
         return
     end
 
-    -- Read this shit later https://gist.github.com/f1ndme/5651385 and the TTT scoreboard
+    for index, ply in ipairs(GAMEMODE:SortPlayers(self.Team)) do
+        local row = vgui.Create("GCScoreboardPlayer", self)
+        row:SetPlayer(ply)
+        row:SetPlayerIndex(index)
+        row:SetTeam(self.Team)
 
-    lp = LocalPlayer()
-    X = ScrW()
-    Y = ScrH()
+        self.PlayerRows[index] = row
+    end
+end
 
-    X2 = X * 0.5
-    Y2 = Y * 0.5
+function gcScoreboardTeamColumn:Paint(w, h)
+    surface.SetDrawColor(self.Team == TEAM_RED and 255 or 0, 0, self.Team == TEAM_BLUE and 255 or 0, 50)
+    surface.DrawRect(0, 0, w, h)
 
-    clr(0, 0, 0, 75)
-    rect(X2 - 400, Y2 - 250, 800, 500)
+    return true
+end
 
-    local clrs = self.ScoreboardColors
-
-    surface.SetDrawColor(clrs.ColorBlue1)
-    surface.DrawRect(X2 - 400, Y2 - 250, 400, 20)
-
-    lgrad(X2 - 399, Y2 - 250, 398, 20, clrs.ColorBlue1, clrs.ColorBlue2, draw.VERTICAL)
-    lgrad(X2 + 1, Y2 - 250, 398, 20, clrs.ColorRed1, clrs.ColorRed2, draw.VERTICAL)
-
-    draw.ShadowText("RED", CW_HUD16, X2 + 10, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
-    draw.ShadowText(ttf(TEAM_RED), CW_HUD16, X2 + 50, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
-    draw.ShadowText("K", CW_HUD16, X2 + 230, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-    draw.ShadowText("D", CW_HUD16, X2 + 260, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-    draw.ShadowText("SCORE", CW_HUD16, X2 + 310, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-    draw.ShadowText("PING", CW_HUD16, X2 + 370, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-
-    draw.ShadowText("BLUE", CW_HUD16, X2 - 390, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
-    draw.ShadowText(ttf(TEAM_BLUE), CW_HUD16, X2 - 340, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
-    draw.ShadowText("K", CW_HUD16, X2 - 170, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-    draw.ShadowText("D", CW_HUD16, X2 - 140, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-    draw.ShadowText("SCORE", CW_HUD16, X2 - 90, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-    draw.ShadowText("PING", CW_HUD16, X2 - 30, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-
-    local myTeam = lp:Team()
-
-    if myTeam == TEAM_RED then
-        draw.ShadowText("ALIVE: " .. (self.AlivePlayers[TEAM_BLUE] or 0), CW_HUD16, X2 - 255, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-    elseif myTeam == TEAM_BLUE then
-        draw.ShadowText("ALIVE: " .. (self.AlivePlayers[TEAM_RED] or 0), CW_HUD16, X2 + 140, Y2 - 240, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+function gcScoreboardTeamColumn:PerformLayout(w, h)
+    if IsValid(self.TitleBar) then
+        self.TitleBar:SetSize(w, h * ROW_HEIGHT_RATIO)
     end
 
-    for k, v in pairs(self:SortPlayers(TEAM_RED)) do
-        clrs.ColorWhite.r = 255
-        clrs.ColorWhite.g = 255
-        clrs.ColorWhite.b = 255
+    for index, row in ipairs(self.PlayerRows) do
+        row:SetSize(w, h * ROW_HEIGHT_RATIO)
+        row:SetPos(0, (h * ROW_HEIGHT_RATIO) * index)
+    end
+end
 
-        n = v:Nick()
+vgui.Register("GCScoreboardTeamColumn", gcScoreboardTeamColumn, "DPanel")
 
-        if v == lp then
-            lgrad(X2 + 1, Y2 + k * 21 - 250, 398, 20, clrs.ColorGray1, clrs.ColorGray2, draw.VERTICAL)
-        else
-            clr(255, 143, 91, 150)
-            rect(X2 + 1, Y2 + k * 21 - 249, 398, 20)
-        end
+local CW_HUD16 = "CW_HUD16"
+local PADDING = 4
+local ALIVE_FORMAT = "ALIVE: %i"
 
-        if myTeam == TEAM_RED and !v:Alive() then
-            clrs.ColorWhite.r = 150
-            clrs.ColorWhite.g = 150
-            clrs.ColorWhite.b = 150
-        end
+local gcScoreboardTitleBar = {}
+AccessorFunc(gcScoreboardTitleBar, "Team", "Team", FORCE_NUMBER)
 
-        draw.ShadowText(k, CW_HUD16, X2 + 10, Y2 - 239 + k * 21, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
-        draw.ShadowText(#n <= 21 and n or sleft(n, 21) .. "...", CW_HUD16, X2 + 40, Y2 - 239 + k * 21, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
-        draw.ShadowText(v:Frags(), CW_HUD16, X2 + 230, Y2 - 239 + k * 21, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-        draw.ShadowText(v:Deaths(), CW_HUD16, X2 + 260, Y2 - 239 + k * 21, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-        draw.ShadowText(v:GetNWInt("GC_SCORE"), CW_HUD16, X2 + 310, Y2 - 239 + k * 21, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-        draw.ShadowText(v:Ping(), CW_HUD16, X2 + 370, Y2 - 239 + k * 21, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+function gcScoreboardTitleBar:Paint(w, h)
+    local colors = GAMEMODE.ScoreboardColors
+    local color1, color2 =
+        self.Team == TEAM_RED and colors.ColorRed1 or colors.ColorBlue1,
+        self.Team == TEAM_RED and colors.ColorRed2 or colors.ColorBlue2
 
-        clrs.ColorWhite.r = 255
-        clrs.ColorWhite.g = 255
-        clrs.ColorWhite.b = 255
+    surface.SetDrawColor(color1)
+    surface.DrawRect(0, 0, w, h)
+    draw.LinearGradient(0, 0, w, h, color1, color2, draw.VERTICAL)
+
+    surface.SetFont(CW_HUD16)
+    local textHeight = select(2, surface.GetTextSize("ALLCAPS"))
+    local vertCenter = h * 0.5 - textHeight * 0.5
+
+    draw.ShadowText(self.Team == TEAM_RED and "RED" or "BLUE", CW_HUD16, PADDING + 6, vertCenter, colors.ColorWhite, colors.ColorBlack, 1, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+    draw.ShadowText(team.TotalFrags(self.Team), CW_HUD16, PADDING + 50, vertCenter, colors.ColorWhite, colors.ColorBlack, 1, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+    draw.ShadowText("K", CW_HUD16, PADDING + 230, vertCenter, colors.ColorWhite, colors.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
+    draw.ShadowText("D", CW_HUD16, PADDING + 260, vertCenter, colors.ColorWhite, colors.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
+    draw.ShadowText("SCORE", CW_HUD16, PADDING + 310, vertCenter, colors.ColorWhite, colors.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
+    draw.ShadowText("PING", CW_HUD16, PADDING + 370, vertCenter, colors.ColorWhite, colors.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
+
+    if LocalPlayer():Team() == self.Team then
+        draw.ShadowText(string.format(ALIVE_FORMAT, GAMEMODE.AlivePlayers[self.Team] or 0), CW_HUD16, PADDING + 140, vertCenter, colors.ColorWhite, colors.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
     end
 
-    for k, v in pairs(self:SortPlayers(TEAM_BLUE)) do
-        clrs.ColorWhite.r = 255
-        clrs.ColorWhite.g = 255
-        clrs.ColorWhite.b = 255
+    return true
+end
 
-        n = v:Nick()
+vgui.Register("GCScoreboardTitleBar", gcScoreboardTitleBar, "DPanel")
 
-        if v == lp then
-            lgrad(X2 + 1 - 400, Y2 + k * 21 - 250, 398, 20, clrs.ColorGray1, clrs.ColorGray2, draw.VERTICAL)
-        else
-            clr(40, 66, 124, 150)
-            rect(X2 + 1 - 400, Y2 + k * 21 - 249, 398, 20)
-        end
+local gcScoreboardPlayer = {}
+AccessorFunc(gcScoreboardPlayer, "Player", "Player")
+AccessorFunc(gcScoreboardPlayer, "PlayerIndex", "PlayerIndex", FORCE_NUMBER)
+AccessorFunc(gcScoreboardPlayer, "Team", "Team", FORCE_NUMBER)
 
-        if myTeam == TEAM_BLUE and !v:Alive() then
-            clrs.ColorWhite.r = 150
-            clrs.ColorWhite.g = 150
-            clrs.ColorWhite.b = 150
-        end
+function gcScoreboardPlayer:Init()
+    self.TextColor = Color(255, 255, 255, 255)
+end
 
-        draw.ShadowText(k, CW_HUD16, X2 - 390, Y2 - 239 + k * 21, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+function gcScoreboardPlayer:Paint(w, h)
+    local lp = LocalPlayer()
+    local n = self.Player:Nick()
+    local colors = GAMEMODE.ScoreboardColors
 
-        draw.ShadowText(#n <= 21 and n or sleft(n, 21) .. "...", CW_HUD16, X2 + 40 - 400, Y2 - 239 + k * 21, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
-        draw.ShadowText(v:Frags(), CW_HUD16, X2 - 170, Y2 - 239 + k * 21, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-        draw.ShadowText(v:Deaths(), CW_HUD16, X2 - 140, Y2 - 239 + k * 21, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-        draw.ShadowText(v:GetNWInt("GC_SCORE"), CW_HUD16, X2 - 90, Y2 - 239 + k * 21, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-        draw.ShadowText(v:Ping(), CW_HUD16, X2 - 30, Y2 - 239 + k * 21, clrs.ColorWhite, clrs.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-
-        clrs.ColorWhite.r = 255
-        clrs.ColorWhite.g = 255
-        clrs.ColorWhite.b = 255
+    if self.Player == lp then
+        draw.LinearGradient(0, 0, w, h, colors.ColorGray1, colors.ColorGray2, draw.VERTICAL)
+    else
+        surface.SetDrawColor(255, 143, 91, 150)
+        surface.DrawRect(0, 0, w, h)
     end
+
+    local yourTeam = lp:Team()
+
+    if yourTeam == self.Team and !self.Player:Alive() then
+        self.TextColor.r = 150
+        self.TextColor.g = 150
+        self.TextColor.b = 150
+    end
+
+    surface.SetFont(CW_HUD16)
+    local textHeight = select(2, surface.GetTextSize("ALLCAPS"))
+    local vertCenter = h * 0.5 - textHeight * 0.5
+
+    draw.ShadowText(self.PlayerIndex, CW_HUD16, PADDING + 10, vertCenter, self.TextColor, colors.ColorBlack, 1, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+    draw.ShadowText(#n <= 21 and n or string.Left(n, 21) .. "...", CW_HUD16, PADDING + 40, vertCenter, self.TextColor, colors.ColorBlack, 1, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+    draw.ShadowText(self.Player:Frags(), CW_HUD16, PADDING + 230, vertCenter, self.TextColor, colors.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
+    draw.ShadowText(self.Player:Deaths(), CW_HUD16, PADDING + 260, vertCenter, self.TextColor, colors.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
+    draw.ShadowText(self.Player:GetNWInt("GC_SCORE"), CW_HUD16, PADDING + 310, vertCenter, self.TextColor, colors.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
+    draw.ShadowText(self.Player:Ping(), CW_HUD16, PADDING + 370, vertCenter, self.TextColor, colors.ColorBlack, 1, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
+
+    self.TextColor.r = 255
+    self.TextColor.g = 255
+    self.TextColor.b = 255
+
+    return true
+end
+
+vgui.Register("GCScoreboardPlayer", gcScoreboardPlayer, "DPanel")
+
+function GM:CreateScoreboard()
+    if IsValid(self.ScoreboardPanel) then
+        return
+    end
+
+    self.ScoreboardPanel = vgui.Create("GCScoreboardFrame")
+    self.ScoreboardPanel:SetSize(800, 500)
+    self.ScoreboardPanel:Center()
+
+    self.ScoreboardPanel.BluePanel = vgui.Create("GCScoreboardTeamColumn", self.ScoreboardPanel)
+    self.ScoreboardPanel.BluePanel:SetTeam(TEAM_BLUE)
+
+    self.ScoreboardPanel.BluePanel.TitleBar = vgui.Create("GCScoreboardTitleBar", self.ScoreboardPanel.BluePanel)
+    self.ScoreboardPanel.BluePanel.TitleBar:SetTeam(TEAM_BLUE)
+
+    self.ScoreboardPanel.RedPanel = vgui.Create("GCScoreboardTeamColumn", self.ScoreboardPanel)
+    self.ScoreboardPanel.RedPanel:SetTeam(TEAM_RED)
+
+    self.ScoreboardPanel.RedPanel.TitleBar = vgui.Create("GCScoreboardTitleBar", self.ScoreboardPanel.RedPanel)
+    self.ScoreboardPanel.RedPanel.TitleBar:SetTeam(TEAM_RED)
+
+    -- Create the player panels
+    self.ScoreboardPanel:Update()
+end
+
+function GM:DestroyScoreboard()
+    if not IsValid(self.ScoreboardPanel) then
+        return
+    end
+
+    self.ScoreboardPanel:Remove()
+end
+
+function GM:UpdateScoreboard()
+    if not IsValid(self.ScoreboardPanel) then
+        return
+    end
+
+    self.ScoreboardPanel:Update()
+end
+
+function GM:HUDDrawScoreBoard()
 end
